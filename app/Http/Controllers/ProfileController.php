52bd85df7e8cdd97c364b5d38d36f16bac3c24f5
Vladyslav Martynenko <vladyslav.martynenko@smartteksas.com>
2022-05-02 20:32:03 +0200

    add edit profile functional

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,64 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
+    use App\Models\User;
+    use Validator;
+
+    class ProfileController extends Controller
+    {
+        //
+        public function __construct()
+        {
+            $this->middleware('auth:api');
+        }
+
+        /**
+         * Get the authenticated User.
+         *
+         * @return \Illuminate\Http\JsonResponse
+         */
+        public function show()
+        {
+            return response()->json(auth()->user());
+        }
+
+        public function edit(Request $request)
+        {
+            $userId = Auth::id();
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|between:2,100',
+                'email' => 'required|string|email|max:100|unique:users,email,' . $userId,
+            ]);
+            if ($validator->fails()) {
+                return response()->json($validator->errors()->toJson(), 400);
+            }
+
+            //If email not have used
+            //debug
+
+            //end debug
+            $user = User::findOrFail($userId);
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+
+
+            if ($user->save()) {
+                return response()->json(
+                    [
+                        'data' => [
+                            'message' => 'User successfully updated',
+                            'access_token' => auth()->user()->getRememberToken()
+                        ]
+                    ]
+                    , 201);
+            } else {
+                return response()->json([
+                    'message' => 'User was not successfully registered',
+                ], 500);
+            }
+
+        }
+    }
